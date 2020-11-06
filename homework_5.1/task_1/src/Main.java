d013fd039e8d0afeac8576f09135c952fe08395a
Dmitry <irlitsa.d.p@gmail.com>
2020-11-06 10:38:49 +0300

    task_1 complete

--- homework_5.1/task_1/src/Main.java
+++ homework_5.1/task_1/src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String phrase = "Каждый охотник желает знать, где сидит фазан";
+
+        String[] wordsPhrase = phrase.split(",?\\s+");
+        System.out.println("Первоначальный массив: " + Arrays.toString(wordsPhrase));
+        for (int i = 0; i < wordsPhrase.length / 2; i++) {
+            String temp = wordsPhrase[i];
+            wordsPhrase[i] = wordsPhrase[wordsPhrase.length - 1 - i];
+            wordsPhrase[wordsPhrase.length - 1 - i] = temp;
+        }
+        System.out.println("Перевернутый массив: " + Arrays.toString(wordsPhrase));
+    }
 }
