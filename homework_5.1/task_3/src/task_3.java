d87c49ffd65600ae2fb1e40202f4f74f210d30c9
Dmitry <irlitsa.d.p@gmail.com>
2020-11-09 13:56:55 +0300

    add task_3

---
+++ homework_5.1/task_3/src/task_3.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class task_3 {
+    public static void main(String[] args) {
+        String[][] arrayX = new String[5][5];
+        for (int i = 0; i < arrayX.length; i++) {
+            for (int j = 0; j < arrayX[i].length; j++) {
+                if (i == j || (((arrayX[i].length - 1) - i) == j)) {
+                    arrayX[i][j] = "X";
+                } else {
+                    arrayX[i][j] = " ";
+                }
+            }
+        }
+        /*for (int i = 0; i < arrayX.length; i++) {
+            for (int j = 0; j < arrayX[i].length; j++) {
+                System.out.print(arrayX[i][j]);
+            }
+            System.out.println();
+        }*/
+    }
+}
