b4910a1f370f267309bc2789a4c084dacbc7f18d
Dmitry <irlitsa.d.p@gmail.com>
2020-11-09 16:46:14 +0300

    task_3 complete update

--- homework_5.1/task_3/src/task_3.java
+++ homework_5.1/task_3/src/task_3.java
@@ -1,9 +1,7 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class task_3 {
     public static void main(String[] args) {
-        String[][] arrayX = new String[5][5];
+        byte depthArray = 7;
+        String[][] arrayX = new String[depthArray][depthArray];
         for (int i = 0; i < arrayX.length; i++) {
             for (int j = 0; j < arrayX[i].length; j++) {
                 if (i == j || (((arrayX[i].length - 1) - i) == j)) {
