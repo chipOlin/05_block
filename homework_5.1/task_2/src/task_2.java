e055090c5827aa1295f1e0b64f3173de6569db2f
Dmitry <irlitsa.d.p@gmail.com>
2020-11-09 23:19:34 +0300

    task_2 complete update

--- homework_5.1/task_2/src/task_2.java
+++ homework_5.1/task_2/src/task_2.java
@@ -3,26 +3,26 @@ import java.util.Arrays;
 public class task_2 {
     public static void main(String[] args) {
         int PATIENT_NUMBER = 30;
-        int MIN_PATIENT_TEMP = 32;
-        int MAX_PATIENT_TEMP = 40;
-        float MIN_HEALTHY_PATIENT = (float) 32.6;
-        float MAX_HEALTHY_PATIENT = (float) 36.9;
+        int MIN_TEMP_PATIENT = 32;
+        int MAX_TEMP_PATIENT = 40;
+        float MIN_TEMP_HEALTHY_PATIENT = 36.2f;
+        float MAX_TEMP_HEALTHY_PATIENT = 36.9f;
 
         float scale = (float) Math.pow(10, 1);
-        float average_temperature = 0;
+        float sumTemperature = 0;
         int num_healthy_patient = 0;
 
-        float[] patiens = new float[PATIENT_NUMBER];
-        for(int i = 0; i <= patiens.length - 1; i++) {
-            float temperature = (float) (Math.random() * (MAX_PATIENT_TEMP - MIN_PATIENT_TEMP)) + MIN_PATIENT_TEMP;
-            patiens[i] = (float) Math.ceil(temperature * scale) / scale;
-            average_temperature += temperature;
-            if(temperature >= MIN_HEALTHY_PATIENT && temperature <= MAX_HEALTHY_PATIENT) {
+        float[] tempPatients = new float[PATIENT_NUMBER];
+        for(int i = 0; i <= tempPatients.length - 1; i++) {
+            float temperature = (float) (Math.random() * (MAX_TEMP_PATIENT - MIN_TEMP_PATIENT)) + MIN_TEMP_PATIENT;
+            tempPatients[i] = (float) Math.ceil(temperature * scale) / scale;
+            sumTemperature += temperature;
+            if(temperature >= MIN_TEMP_HEALTHY_PATIENT && temperature <= MAX_TEMP_HEALTHY_PATIENT) {
                 num_healthy_patient++;
             }
         }
-        System.out.println("Температуры пациентов: " + Arrays.toString(patiens));
-        System.out.println("Средняя температура: " + Math.ceil((average_temperature / PATIENT_NUMBER) * scale) / scale);
+        System.out.println("Температуры пациентов: " + Arrays.toString(tempPatients).replaceAll("[\\[\\]]",""));
+        System.out.println("Средняя температура: " + Math.ceil((sumTemperature / PATIENT_NUMBER) * scale) / scale);
         System.out.println("Количество здоровых: " + num_healthy_patient);
     }
 }
