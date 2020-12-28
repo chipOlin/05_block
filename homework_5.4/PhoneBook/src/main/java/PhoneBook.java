69cd0b7a5c83650a17da9ced815f8fdca7c3fd20
Dmitry <irlitsa.d.p@gmail.com>
2020-12-28 23:51:53 +0300

    add task PhoneBook

--- homework_5.4/PhoneBook/src/main/java/PhoneBook.java
+++ homework_5.4/PhoneBook/src/main/java/PhoneBook.java
@@ -11,13 +11,15 @@ public class PhoneBook {
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона
         // если такой номер уже есть в списке, то перезаписать имя абонента
+        System.out.println(getNameByPhone(phone));
         phoneBook.put(name, phone);
     }
 
     public String getNameByPhone(String phone) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
-        return "";
+        if (phoneBook.containsValue(phone)) return "exist";
+        else return "not exist";
     }
 
     public Set<String> getPhonesByName(String name) {
